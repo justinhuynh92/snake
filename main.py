f63f18cd8ada5a436ab932f25aa7ce6f537894c4
Justin Huynh <justinhuynh639@gmail.com>
2023-10-19 13:33:48 -0700

    snake displayed correctly

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pygame as pg
 from random import randrange
 
 WINDOW = 1000
+TILE_SIZE = 50
+RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
+snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
+snake.center = get_random_position()
+length = 1
+segments = [snake.copy()]
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
@@ -10,5 +17,7 @@ while True:
         if event.type == pg.QUIT:
             exit()
     screen.fill('black')
+    # draw snake
+    [pg.draw.rect(screen, 'green', segment) for segment in segments]
     pg.display.flip()
     clock.tick(60)
