a56aff5f190f8fe29392ad819560eb0a3815b7f3
Justin Huynh <justinhuynh639@gmail.com>
2023-10-19 13:39:28 -0700

    move in place

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ snake = pg.rect.Rect([0, 0, TILE_SIZE - 2, TILE_SIZE - 2])
 snake.center = get_random_position()
 length = 1
 segments = [snake.copy()]
+snake_dir = (0, 0)
 screen = pg.display.set_mode([WINDOW] * 2)
 clock = pg.time.Clock()
 
@@ -16,8 +17,21 @@ while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_w:
+                snake_dir = (0, -TILE_SIZE)
+            if event.key == pg.K_s:
+                snake_dir = (0, TILE_SIZE)
+            if event.key == pg.K_a:
+                snake_dir = (-TILE_SIZE, 0)
+            if event.key == pg.K_d:
+                snake_dir = (TILE_SIZE, 0)
     screen.fill('black')
     # draw snake
     [pg.draw.rect(screen, 'green', segment) for segment in segments]
+    # move snake
+    snake.move_ip(snake_dir)
+    segments.append(snake.copy())
+    segments = segments[-length:]
     pg.display.flip()
     clock.tick(60)
