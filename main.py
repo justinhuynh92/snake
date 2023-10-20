b780f45f6520cedce088f4621a17af5a984dd546
Justin Huynh <justinhuynh639@gmail.com>
2023-10-20 11:55:53 -0700

    selfeating

--- main.py
+++ main.py
@@ -30,6 +30,16 @@ while True:
             if event.key == pg.K_d:
                 snake_dir = (TILE_SIZE, 0)
     screen.fill('black')
+    # check border and selfeating
+    self_eating = pg.Rect.collidelist(snake, segments[:-1]) != -1
+    if snake.left < 0 or snake.right > WINDOW or snake.top < 0 or snake.bottom > WINDOW or self_eating:
+        snake.center, food.center = get_random_position(), get_random_position()
+        length, snake_dir = 1,(0, 0)
+        segments = [snake.copy()]
+    # check food
+    if snake.center == food.center:
+        food.center = get_random_position()
+        length += 1
     # draw food
     pg.draw.rect(screen, 'red', food)
     # draw snake
